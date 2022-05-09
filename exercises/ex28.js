dd7713779ba7383d51749e906a3b6c1ab2b7d480
Megumi Akama <kamayatu91@gmail.com>
2022-05-09 16:23:59 -0700

    Solved ex26 to 28

--- exercises/ex28.js
+++ exercises/ex28.js
@@ -11,3 +11,29 @@ deleteMessage(index) - removes the message corresponding to the index provided
 addFriend() - increases the friend count by 1
 removeFriend() - decreases the friend count by 1
 */
+
+const facebookProfile = {
+  name: "Megumi",
+  numberOfFriends: 9,
+  messages: ["Hi", "Sleepy", "I'm studying right now."],
+  postMessage: function (message) {
+     facebookProfile.messages.push(message);
+     return facebookProfile.messages;
+  },
+  deleteMessage: function (index) {
+     facebookProfile.messages.splice(index, 1);
+     return facebookProfile.messages;
+  },
+  addFriend: function () {
+     facebookProfile.numberOfFriends++;
+     return facebookProfile.numberOfFriends;
+  },
+  removeFriend: function () {
+    facebookProfile.numberOfFriends--;
+     return facebookProfile.numberOfFriends;
+  }
+};
+
+// console.log(facebookProfile.postMessage("test"));
+// console.log(facebookProfile.deleteMessage(1));
+// console.log(facebookProfile.removeFriend());
